699da4542ed0a3b376d0a41f19178dcaa85d6956
rmhirota <re.hirota@gmail.com>
2021-08-13 10:11:00 -0300

    tira óbitos do app

--- app/app.R
+++ app/app.R
@@ -16,12 +16,6 @@ ef_int_covid <-  glmmTMB::fixef(mod_int_covid)$cond %>%
   dplyr::bind_rows() %>%
   janitor::clean_names()
 
-# Óbitos Covid
-mod_ob_covid <- readr::read_rds("data/modelo_obitos_covid.rds")
-ef_ob_covid <-  glmmTMB::fixef(mod_ob_covid)$cond %>%
-  dplyr::bind_rows() %>%
-  janitor::clean_names()
-
 da_clustered <- readr::read_rds("data/da_clustered.rds")
 
 muni_vulneraveis <- da_clustered %>%
@@ -33,7 +27,7 @@ muni_vulneraveis <- dados %>%
 # ui ---------------------------------------------------------------------------
 
 ui <- dashboardPage(
-  skin = "green",
+  skin = "red",
 
   # title ----
   dashboardHeader(title = "Covid e Poluição"),
@@ -41,7 +35,6 @@ ui <- dashboardPage(
   dashboardSidebar(sidebarMenu(
     id = "sidebarid",
     menuItem("Internações", tabName = "page1"),
-    menuItem("Óbitos", tabName = "page2"),
     shinyWidgets::pickerInput(
       "mes", "Mês",
       choices = 1:12,
@@ -114,41 +107,7 @@ ui <- dashboardPage(
           )
         )
       )
-    ),
-
-    # óbitos ----
-    tabItem(
-      tabName = "page2",
-      fluidRow(
-        shinydashboard::tabBox(
-          width = 12,
-          id = "obitos",
-          title = "Efeito de dias acima de 25 por mês em óbitos",
-
-          shiny::tabPanel(
-            "Geral e UF",
-            div(
-              h4("Geral"),
-              reactable::reactableOutput("ef_pol_geral_obitos")
-            ),
-            div(
-              h4("Por UF"),
-              reactable::reactableOutput("ef_pol_uf_obitos")
-            )
-          ),
-          shiny::tabPanel(
-            "Municípios vulneráveis",
-            reactable::reactableOutput("ef_pol_vulneraveis_obitos"),
-          ),
-          shiny::tabPanel(
-            "Todos os municípios",
-            reactable::reactableOutput("ef_pol_municipios_obitos"),
-            downloadButton("download_obitos", "Download")
-          )
-        )
-      )
     )
-
   ))
 )
 
@@ -193,21 +152,6 @@ server <- function(input, output, session) {
         )
       )
   })
-  output$ef_pol_geral_obitos <- reactable::renderReactable({
-    dados %>%
-      dplyr::filter(mes %in% input$mes, uf %in% input$uf) %>%
-      dplyr::summarise(media_dias = round(mean(dias_acima_25), 2)) %>%
-      dplyr::mutate(
-        pct_ob_covid = exp(ef_ob_covid$dias_acima_25)^media_dias - 1,
-        pct_ob_covid = scales::percent(pct_ob_covid, .1)
-      ) %>%
-      reactable::reactable(
-        columns = list(
-          media_dias = reactable::colDef("Média de dias acima de 25"),
-          pct_ob_covid = reactable::colDef("% a mais de óbitos Covid")
-        )
-      )
-  })
 
   output$ef_pol_uf <- reactable::renderReactable({
     da_uf() %>%
@@ -224,21 +168,6 @@ server <- function(input, output, session) {
           media_dias = reactable::colDef("Média de dias acima de 25"),
           pct_int_srag = reactable::colDef("% a mais de internações SRAG"),
           pct_int_covid = reactable::colDef("% a mais de internações Covid")
-        )
-      )
-  })
-  output$ef_pol_uf_obitos <- reactable::renderReactable({
-    da_uf() %>%
-      dplyr::mutate(
-        pct_ob_covid = exp(ef_ob_covid$dias_acima_25)^media_dias - 1,
-        pct_ob_covid = scales::percent(pct_ob_covid, .1)
-      ) %>%
-      dplyr::arrange(dplyr::desc(media_dias)) %>%
-      reactable::reactable(
-        columns = list(
-          uf = reactable::colDef("UF"),
-          media_dias = reactable::colDef("Média de dias acima de 25"),
-          pct_ob_covid = reactable::colDef("% a mais de óbitos Covid")
         )
       )
   })
@@ -260,23 +189,6 @@ server <- function(input, output, session) {
           dias_acima_25 = reactable::colDef("Média de dias acima de 25 (jul-out)"),
           pct_int_srag = reactable::colDef("% a mais de internações SRAG"),
           pct_int_covid = reactable::colDef("% a mais de internações Covid")
-        )
-      )
-  })
-  output$ef_pol_vulneraveis_obitos <- reactable::renderReactable({
-    da_vulneraveis() %>%
-      dplyr::mutate(
-        pct_ob_covid = exp(ef_ob_covid$dias_acima_25)^dias_acima_25 - 1,
-        pct_ob_covid = scales::percent(pct_ob_covid, .1)
-      ) %>%
-      dplyr::relocate(dias_acima_25, .before = pct_ob_covid) %>%
-      dplyr::arrange(dplyr::desc(dias_acima_25)) %>%
-      reactable::reactable(
-        columns = list(
-          code_muni = reactable::colDef("Código IBGE"),
-          muni_nm = reactable::colDef("Município"),
-          dias_acima_25 = reactable::colDef("Média de dias acima de 25 (jul-out)"),
-          pct_ob_covid = reactable::colDef("% a mais de óbitos Covid")
         )
       )
   })
@@ -313,25 +225,6 @@ server <- function(input, output, session) {
         filterable = TRUE
       )
   })
-  output$ef_pol_municipios_obitos <- reactable::renderReactable({
-    da_municipios() %>%
-      dplyr::mutate(
-        pct_ob_covid = exp(ef_ob_covid$dias_acima_25)^dias_acima_25 - 1,
-        pct_ob_covid = scales::percent(pct_ob_covid, .1)
-      ) %>%
-      dplyr::relocate(dias_acima_25, .before = pct_ob_covid) %>%
-      dplyr::arrange(dplyr::desc(dias_acima_25)) %>%
-      reactable::reactable(
-        columns = list(
-          code_muni = reactable::colDef("Código IBGE"),
-          muni_nm = reactable::colDef("Município"),
-          uf = reactable::colDef("UF"),
-          dias_acima_25 = reactable::colDef("Média de dias acima de 25 (jul-out)"),
-          pct_ob_covid = reactable::colDef("% a mais de óbitos Covid")
-        ),
-        filterable = TRUE
-      )
-  })
 
   output$download <- downloadHandler(
     filename = "srag_covid_poluicao.csv",
@@ -355,18 +248,6 @@ server <- function(input, output, session) {
           pct_int_covid = exp(ef_int_covid$dias_acima_25)^media_dias - 1
         ) %>%
         dplyr::arrange(dplyr::desc(media_dias)) %>%
-        readr::write_csv(file)
-    }
-  )
-  output$download_obitos <- downloadHandler(
-    filename = "srag_covid_poluicao_obitos.csv",
-    content = function(file) {
-      da_municipios() %>%
-        dplyr::mutate(
-          pct_ob_covid = exp(ef_ob_covid$dias_acima_25)^dias_acima_25 - 1
-        ) %>%
-        dplyr::relocate(dias_acima_25, .before = pct_ob_covid) %>%
-        dplyr::arrange(dplyr::desc(dias_acima_25)) %>%
         readr::write_csv(file)
     }
   )
